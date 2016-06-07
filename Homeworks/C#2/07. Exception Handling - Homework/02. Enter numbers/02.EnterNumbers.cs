d2d9c5f2386cf859d63a6b2eb04a1b2cab429fc4
Kaloyan Kostov <kaloyanit@yahoo.com>
2016-06-07 21:04:03 +0300

    C#2- Done

--- Homeworks/C#2/07. Exception Handling - Homework/02. Enter numbers/02.EnterNumbers.cs
+++ Homeworks/C#2/07. Exception Handling - Homework/02. Enter numbers/02.EnterNumbers.cs
@@ -15,27 +15,45 @@
         {
             //ReadNumber(5, 15);
             Read10Numbers(0, 100);
-        }
-
-        static void ReadNumber(int start, int end)
-        {
-            int number = int.Parse(Console.ReadLine());
-            if (number < start || number > end)
-            {
-                throw new Exception("Invalid number");
-            }
-        }
+        }        
 
         static void Read10Numbers(int start, int end)
         {
             int[] array = new int[10];
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = int.Parse(Console.ReadLine());
-                if (array[i] < start || array[i] > end)
+
+                try
                 {
-                    throw new Exception("Exception");
+                    array[i] = int.Parse(Console.ReadLine());
                 }
+                catch (FormatException)
+                {
+                    throw new FormatException();
+                }                
+            }
+            bool forPrint = true;
+            for (int a = 0; a < array.Length - 1; a++)
+            {
+                if (array[a] < start || array[a] > end || array[a] > array[a + 1])
+                {
+                    forPrint = false;
+                    //throw new Exception();
+                    Console.WriteLine("Exception");
+                    break;
+                }                            
+            }
+            if (forPrint)
+            {
+                Console.Write("1 < ");
+                for (int b = 0; b < array.Length; b++)
+                {
+                    
+                    
+                        Console.Write(array[b] + " < ");
+                    
+                }
+                Console.Write("100");
             }
         }
     }
