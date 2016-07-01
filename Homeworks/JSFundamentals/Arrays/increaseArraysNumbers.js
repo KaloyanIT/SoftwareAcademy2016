26c2c99f902828a225c77fbd0317900f1b132912
KaloyanIT <kaloyanit@yahoo.com>
2016-07-01 11:54:43 +0300

    added some resolved task and workshop

--- Homeworks/JSFundamentals/Arrays/increaseArraysNumbers.js
+++ Homeworks/JSFundamentals/Arrays/increaseArraysNumbers.js
@@ -1,15 +1,10 @@
-var array = [];
+function solve(number) {
+    var array = [];
+    for(var i = 0; i < number; i++){
+        array[i] = i * 5;
+    }
 
-
-
-for (var i = 0; i < 20; i++) {
-    array[i] = i + 1;
-}
-
-for (var a = 0; a < array.length; a++) {
-    array[a] *= 5;
-}
-
-for (var o = 0; o < array.length ; o++) {
-    console.log(array[o]);
+    for (var o = 0; o < array.length ; o++) {
+        console.log(array[o]);
+    }
 }
--- Homeworks/JSFundamentals/Arrays/lexicographicallyComparison.js
+++ Homeworks/JSFundamentals/Arrays/lexicographicallyComparison.js
@@ -1,18 +1,33 @@
-Array.prototype.compareLexic = function (array) {
-    for (var i = 0; i < Math.min(this.length, array.length); i++) {
-        if (this[i] !== array[i]) {
-            return this[i] < array[i] ? -1 : 1;
+// Array.prototype.compareLexic = function (array) {
+//     for (var i = 0; i < Math.min(this.length, array.length); i++) {
+//         if (this[i] !== array[i]) {
+//             return this[i] < array[i] ? ">" : "<";
+//         }
+//     }
+//
+//     if (this.length !== array.length) {
+//         return this.length < array.length ? ">" : "<";
+//     }
+//
+//     return "=";
+//
+// };
+
+function solve(array) {
+    var array1 = array[0];
+    var array2 = array[1];
+
+    for (var i = 0; i < Math.min(array1.length, array2.length); i++) {
+        if (array1[i] !== array2[i]) {
+            return array1[i] < array2[i] ? "<" : ">";
         }
     }
 
-    if (this.length !== array.length) {
-        return this.length < array.length ? -1 : 1;
+    if (array1.length !== array2.length) {
+        return array1.length < array2.length ? "<" : ">";
     }
 
-    return 0;
+    return "=";
+}
 
-};
-
-console.log(['a', 'b', 'c'].compareLexic(['a', 'b', 'c']));
-console.log(['a', 'b', 'c'].compareLexic(['a', 'c', 'b']));
-console.log(['a', 'c', 'b'].compareLexic(['a', 'b', 'c']));
+console.log(solve(['food', 'food']));
